6b6adcbda2309f6c3486771406624613d37f85ae
piyush <piyush@piyush>
2019-08-19 00:51:08 +0530

    demo

--- colourclassesJava/src/main/java/com/colour/module/master/dao/ICityDao.java
+++ colourclassesJava/src/main/java/com/colour/module/master/dao/ICityDao.java
@@ -3,8 +3,9 @@ package com.colour.module.master.dao;
 import java.util.List;
 
 import com.colour.model.CityMaster;
-
+//city Master
 public interface ICityDao {
 
+	
 	public List<CityMaster> displayCityDao();
 }
